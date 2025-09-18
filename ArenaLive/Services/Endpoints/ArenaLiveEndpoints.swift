9f142e50fd37f2cf10bbce624e5d8653b65abe34
ArilsonFJS <arilsonsilva61@gmail.com>
2025-09-18 17:57:20 -0300

    implementando ArenaLiveEndpoints

--- ArenaLive/Services/Endpoints/ArenaLiveEndpoints.swift
+++ ArenaLive/Services/Endpoints/ArenaLiveEndpoints.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum ArenaLiveEndpoints: Endpoint {
+    
+    case competitions
+    case teams(competitionId: Int)
+    case matches(teamId: Int)
+    
+    var path: String {
+        switch self {
+        case .competitions:
+            return "competitions"
+        case .teams(let competitionId):
+            return "completitions/\(competitionId)/teams"
+        case .matches(let teamId):
+            return "teams/\(teamId)/matches"
+        }
+    }
+}
