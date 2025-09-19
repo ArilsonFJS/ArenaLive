f5d15c47ab3860da39fbd58bbca5861104752a90
ArilsonFJS <arilsonsilva61@gmail.com>
2025-09-19 18:33:18 -0300

    criando servico de request generica

--- ArenaLive/Services/Network/RequestManager.swift
+++ ArenaLive/Services/Network/RequestManager.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+struct RequestManager: RequestManaging {
+    
+    let session: URLSession
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func request<T: Decodable>(endpoint: any Endpoint, completion: @escaping (Result<T,APIError>) -> Void) {
+        guard let url = URL(string: endpoint.fullURL) else { return completion(.failure(APIError.invalidURL))}
+        
+        let task = session.dataTask(with: URLRequest.init(url: url)) { data, response, error in
+            
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(APIError.requestFailed(error)))
+                }
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                DispatchQueue.main.async {
+                    completion(.failure(.invalidResponse))
+                }
+                return
+            }
+            
+            guard let jsonData = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(.invalidResponse))
+                }
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decoded = try decoder.decode(T.self, from: jsonData)
+                completion(.success(decoded))
+            }catch let error {
+                completion(.failure(.decodingFailed(error)))
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
